7c42612bbe3f95a6d9fe2568a29040db613a4c8a
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2023-02-09 09:03:16 -0300

    feat: finish exercise 06 extra 1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,9 +3,13 @@
 
 import * as React from 'react'
 
+function formatMediaDebugValue({query, state}) {
+  return `query: ${query}, state: ${state}`
+}
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  React.useDebugValue(query)
+  React.useDebugValue({query, state}, formatMediaDebugValue)
 
   React.useEffect(() => {
     const mql = window.matchMedia(query)
